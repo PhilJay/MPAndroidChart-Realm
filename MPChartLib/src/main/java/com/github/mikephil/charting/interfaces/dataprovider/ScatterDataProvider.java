69db47889dd46b08956906e7e93602ccab44de1e
Philipp Jahoda <phil.jahoda@gmail.com>
2016-07-01 18:45:03 +0200

    Fix dependency issues

--- MPChartLib/src/main/java/com/github/mikephil/charting/interfaces/dataprovider/ScatterDataProvider.java
+++
@@ -1,9 +0,0 @@
-package com.github.mikephil.charting.interfaces.dataprovider;
-
-import com.github.mikephil.charting.data.ScatterData;
-import com.github.mikephil.charting.renderer.scatter.ShapeRenderer;
-
-public interface ScatterDataProvider extends BarLineScatterCandleBubbleDataProvider {
-
-    ScatterData getScatterData();
-}
