e0d0037e511c610fcbecf05135657f9bca869d79
Philipp Jahoda <phil.jahoda@gmail.com>
2016-07-02 12:40:51 +0200

    Fixes related to performance and GC

--- MPChartLib-Realm/src/main/java/com/github/mikephil/charting/data/realm/implementation/RealmLineDataSet.java
+++ MPChartLib-Realm/src/main/java/com/github/mikephil/charting/data/realm/implementation/RealmLineDataSet.java
@@ -253,6 +253,11 @@ public class RealmLineDataSet<T extends RealmObject> extends RealmLineRadarDataS
     }
 
     @Override
+    public int getCircleColorCount() {
+        return mCircleColors.size();
+    }
+
+    @Override
     public int getCircleColor(int index) {
         return mCircleColors.get(index % mCircleColors.size());
     }
